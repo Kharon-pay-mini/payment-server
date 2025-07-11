adaf369760d48d9d8bcc080d970b5f7f68c6807d
okolievans <ebounce500@gmail.com>
2025-07-11 03:01:14 +0100

    fix: change all user_id columns from VARCHAR(50) to TEXT

--- migrations/2025-07-05-151131_create_transactions_table/up.sql
+++ migrations/2025-07-05-151131_create_transactions_table/up.sql
@@ -4,7 +4,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS transactions (
     tx_id UUID PRIMARY KEY DEFAULT (uuid_generate_v4()),
-    user_id VARCHAR NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    user_id TEXT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     order_type VARCHAR(10) NOT NULL,
     crypto_amount DECIMAL(18, 8) NOT NULL DEFAULT 0.0,
     crypto_type VARCHAR(10) NOT NULL,
