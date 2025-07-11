224ff3b5dceddd4965e73ab2cfc61e154f301bf4
okolievans <ebounce500@gmail.com>
2025-07-11 01:06:05 +0100

    fix: clean up migrations and resolve FK constraint

--- migrations/2025-06-27-144915_create_session_controller_info/up.sql
+++ migrations/2025-06-27-144915_create_session_controller_info/up.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE session_controller_info (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id VARCHAR NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    user_id VARCHAR(50) NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     username VARCHAR(50) NOT NULL,
     controller_address VARCHAR(64) NOT NULL,
     session_policies JSONB NOT NULL,
