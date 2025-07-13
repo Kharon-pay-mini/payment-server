a23e23566d47a7ce2686063642b42d0c296b1349
okolievans <ebounce500@gmail.com>
2025-07-13 12:52:34 +0100

    debug: fixing db errors

--- src/models/schema.rs
+++ src/models/schema.rs
@@ -4,7 +4,8 @@ diesel::table! {
     otp (otp_id) {
         otp_id -> Uuid,
         otp_code -> Int4,
-        user_id -> Text,
+        #[max_length = 50]
+        user_id -> Varchar,
         created_at -> Timestamptz,
         expires_at -> Timestamptz,
     }
@@ -60,11 +61,44 @@ diesel::table! {
 diesel::table! {
     user_bank_account (id) {
         id -> Uuid,
-        user_id -> Text,
+        #[max_length = 50]
+        user_id -> Varchar,
         #[max_length = 255]
         bank_name -> Varchar,
         #[max_length = 50]
         account_number -> Varchar,
+        created_at -> Nullable<Timestamptz>,
+        updated_at -> Nullable<Timestamptz>,
+    }
+}
+
+diesel::table! {
+    user_security_logs (log_id) {
+        log_id -> Uuid,
+        #[max_length = 50]
+        user_id -> Varchar,
+        #[max_length = 50]
+        ip_address -> Varchar,
+        #[max_length = 50]
+        city -> Varchar,
+        #[max_length = 50]
+        country -> Varchar,
+        failed_login_attempts -> Int4,
+        flagged_for_review -> Bool,
+        created_at -> Nullable<Timestamptz>,
+    }
+}
+
+diesel::table! {
+    user_wallet (id) {
+        #[max_length = 50]
+        id -> Varchar,
+        #[max_length = 50]
+        user_id -> Varchar,
+        #[max_length = 100]
+        wallet_address -> Nullable<Varchar>,
+        #[max_length = 50]
+        network_used_last -> Nullable<Varchar>,
         created_at -> Nullable<Timestamptz>,
         updated_at -> Nullable<Timestamptz>,
     }
@@ -90,11 +124,15 @@ diesel::joinable!(otp -> users (user_id));
 diesel::joinable!(session_controller_info -> users (user_id));
 diesel::joinable!(transactions -> users (user_id));
 diesel::joinable!(user_bank_account -> users (user_id));
+diesel::joinable!(user_security_logs -> users (user_id));
+diesel::joinable!(user_wallet -> users (user_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
     otp,
     session_controller_info,
     transactions,
     user_bank_account,
+    user_security_logs,
+    user_wallet,
     users,
 );
